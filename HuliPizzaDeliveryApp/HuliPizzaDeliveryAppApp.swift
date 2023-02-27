61efa0c44b76d203ce62c23a2a5190d6e6467b52
Abishan <highoncode09@gmail.com>
2023-02-27 19:05:23 +0530

    Initial Commit

---
+++ HuliPizzaDeliveryApp/HuliPizzaDeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaDeliveryAppApp.swift
+//  HuliPizzaDeliveryApp
+//
+//  Created by Abishan Parameswaran on 2023-02-27.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaDeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
